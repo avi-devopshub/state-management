3a971d1d62c751ba199ebb8a38bb9adf43a5aa7d
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-08 13:47:33 +0530

    Add AWS S3 bucket and EC2 instance configuration to Terraform files

--- provider.tf
+++ provider.tf
@@ -11,3 +11,14 @@ provider "aws" {
   region = "ap-south-2"
   profile = "dev"
 }
+
+terraform {
+    backend "s3" {
+        bucket = "avinash-s3-remotebackend-08092026"
+        region = "ap-south-2"
+        profile = "dev"
+        share_credentials_files = ["/root/.aws/credentials"]
+        key = "terraform.tfstate"
+        use_lockfile = true
+    }
+}
