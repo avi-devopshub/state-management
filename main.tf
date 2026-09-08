bb83521e3f4fed95d6de2a321b96589af4bb5bf7
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-08 14:02:15 +0530

    Add S3 bucket configuration and provider settings in separate files

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "aws_s3_bucket" "s3" {
-    bucket = "avinash-s3-remotebackend-08092026"
-    tags = {
-        Name = "avinash-s3-remotebackend-08092026"
-    }
-}
 #Public EC2
 resource "aws_instance" "public_ec2" {
     ami = "ami-0199ac7c9fbf9ed83"
---
+++ s3/main.tf
@@ -0,0 +1,6 @@
+resource "aws_s3_bucket" "s3" {
+    bucket = "avinash-s3-remotebackend-08092026"
+    tags = {
+        Name = "avinash-s3-remotebackend-08092026"
+    }
+}
