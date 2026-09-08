921570ef5d08b9f89be67473c23134b00c22bb1d
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-08 17:11:57 +0530

    Update EC2 instance configuration to ensure correct instance count

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "public_ec2" {
     ami = "ami-0199ac7c9fbf9ed83"
     instance_type = "t3.micro"
     key_name = "hyderabad"
+    count = 2
     tags = {
         Name = "public-ec2"
     }
